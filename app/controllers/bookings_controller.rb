a8bd512a36f29f2e80f687fc9b5b359fcfc580c2
alix capely <alixcapely@gmail.com>
2023-11-22 15:39:16 +0100

    OK booking V1

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,47 +1,37 @@
 class BookingsController < ApplicationController
 
-  # def new
-  #   @pony = Pony.new
-  # end
-
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
+    @pony = Pony.find(params[:pony_id])
+    @booking.pony = @pony
+    @booking.total_price = @pony.price_per_day * (@booking.end_date - @booking.start_date)
     if @booking.save!
-      redirect_to pony_path(@pony)
+      # redirect_to pony_path(@pony)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-    if @booking.update(booking_params)
-      redirect_to booking_path(@booking)
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @booking.update(booking_params)
+  #     redirect_to booking_path(@booking)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def destroy
-    @booking.destroy
-    redirect_to booking_path, status: :see_other
-  end
-
-
+  # def destroy
+  #   @booking.destroy
+  #   redirect_to booking_path, status: :see_other
+  # end
 
   private
 
-  # def set_pony
-  #   @pony = Pony.find(params[:id])
-  # end
-
-  # def pony_params
-  #   params.require(:pony).permit(:name, :race, :location, :birth_date, :sex, :purpose, :coat, :price_per_day, photos: [])
-  # end
-end
-
-
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
