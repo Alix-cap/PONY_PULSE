1671f488c309078a85136f93254f9509bef03125
pierre guitard <pierreguitard78@gmail.com>
2023-11-22 14:51:34 +0100

    profile and delete of pony

--- app/controllers/ponies_controller.rb
+++ app/controllers/ponies_controller.rb
@@ -1,5 +1,5 @@
 class PoniesController < ApplicationController
-  before_action :set_pony, only: [:show]
+  before_action :set_pony, only: [:show, :destroy]
 
   def show
   end
@@ -12,13 +12,19 @@ class PoniesController < ApplicationController
     @pony = Pony.new(pony_params)
     @pony.user = current_user
     if @pony.save!
-      redirect_to root_path  # update to pony_path(@pony) when available
+      redirect_to profile_path(current_user)  # update to pony_path(@pony) when available
     else
       render :new, status: :unprocessable_entity
     end
+  end
 
   def index
     @ponies = Pony.all
+  end
+
+  def destroy
+    @pony.destroy
+    redirect_to profile_path(current_user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  resources :ponies, only: [:new, :create, :index, :show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "pages#home", as: "home"
+  resources :ponies, only: [:new, :create, :index, :show, :destroy]
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "profile", to: "pages#profile", as: "profile"
 end
