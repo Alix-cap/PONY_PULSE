aa2d6c7f67bc6b9a6e40c2c4e07bca87216e2b12
Romain Nicault <romain@nicault.fr>
2023-11-22 12:16:45 +0100

    user story : done

--- app/controllers/ponies_controller.rb
+++ app/controllers/ponies_controller.rb
@@ -4,9 +4,27 @@ class PoniesController < ApplicationController
   def show
   end
 
+  def new
+    @pony = Pony.new
+  end
+
+  def create
+    @pony = Pony.new(pony_params)
+    @pony.user = current_user
+    if @pony.save!
+      redirect_to root_path  # update to pony_path(@pony) when available
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_pony
     @pony = Pony.find(params[:id])
+  end
+
+  def pony_params
+    params.require(:pony).permit(:name, :race, :location, :birth_date, :sex, :purpose, :coat, :price_per_day, photos: [])
   end
 end
